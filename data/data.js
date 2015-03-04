12bf3957a48db98fae1e102e23350c4ebb90b2b4
abenrob <adamben.roberts@gmail.com>
2015-03-04 18:28:48 +0100

    scrollls and tweens

--- data/data.js
+++ data/data.js
@@ -1,5 +1,6 @@
 var data = [
   {
+    "pos":0,
     "year":2014,
     "week":48,
     "headline": "Activité faible",
@@ -7,6 +8,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":138},{"region":"Aquitaine","cases":87},{"region":"Auvergne","cases":21},{"region":"Basse-Normandie","cases":48},{"region":"Bourgogne","cases":0},{"region":"Bretagne","cases":34},{"region":"Centre","cases":38},{"region":"Champagne-Ardenne","cases":71},{"region":"Corse","cases":22},{"region":"Franche-Comte","cases":56},{"region":"Haute-Normandie","cases":0},{"region":"Languedoc-Roussillon","cases":34},{"region":"Limousin","cases":76},{"region":"Lorraine","cases":4},{"region":"Midi-Pyrenees","cases":19},{"region":"Nord-Pas-de-Calais","cases":44},{"region":"Pays-de-la-Loire","cases":42},{"region":"Picardie","cases":46},{"region":"Poitou-Charentes","cases":7},{"region":"Provence-Alpes-Cote-dAzur","cases":25},{"region":"Ile-de-France","cases":79},{"region":"Rhone-Alpes","cases":57}]
   },
   {
+    "pos":1,
     "year":2014,
     "week":49,
     "headline": "Activité faible en augmentation",
@@ -14,6 +16,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":82},{"region":"Aquitaine","cases":36},{"region":"Auvergne","cases":7},{"region":"Basse-Normandie","cases":55},{"region":"Bourgogne","cases":29},{"region":"Bretagne","cases":83},{"region":"Centre","cases":42},{"region":"Champagne-Ardenne","cases":125},{"region":"Corse","cases":37},{"region":"Franche-Comte","cases":0},{"region":"Haute-Normandie","cases":49},{"region":"Languedoc-Roussillon","cases":33},{"region":"Limousin","cases":82},{"region":"Lorraine","cases":49},{"region":"Midi-Pyrenees","cases":10},{"region":"Nord-Pas-de-Calais","cases":56},{"region":"Pays-de-la-Loire","cases":63},{"region":"Picardie","cases":0},{"region":"Poitou-Charentes","cases":23},{"region":"Provence-Alpes-Cote-dAzur","cases":20},{"region":"Ile-de-France","cases":98},{"region":"Rhone-Alpes","cases":92}]
   },
   {
+    "pos":2,
     "year":2014,
     "week":50,
     "headline": "Activité faible en augmentation",
@@ -21,6 +24,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":49},{"region":"Aquitaine","cases":70},{"region":"Auvergne","cases":33},{"region":"Basse-Normandie","cases":79},{"region":"Bourgogne","cases":116},{"region":"Bretagne","cases":62},{"region":"Centre","cases":65},{"region":"Champagne-Ardenne","cases":38},{"region":"Corse","cases":52},{"region":"Franche-Comte","cases":67},{"region":"Haute-Normandie","cases":90},{"region":"Languedoc-Roussillon","cases":29},{"region":"Limousin","cases":94},{"region":"Lorraine","cases":38},{"region":"Midi-Pyrenees","cases":16},{"region":"Nord-Pas-de-Calais","cases":220},{"region":"Pays-de-la-Loire","cases":45},{"region":"Picardie","cases":0},{"region":"Poitou-Charentes","cases":43},{"region":"Provence-Alpes-Cote-dAzur","cases":24},{"region":"Ile-de-France","cases":107},{"region":"Rhone-Alpes","cases":76}]
   },
   {
+    "pos":3,
     "year":2014,
     "week":51,
     "headline": "Activité faible",
@@ -28,6 +32,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":17},{"region":"Aquitaine","cases":80},{"region":"Auvergne","cases":87},{"region":"Basse-Normandie","cases":59},{"region":"Bourgogne","cases":37},{"region":"Bretagne","cases":45},{"region":"Centre","cases":65},{"region":"Champagne-Ardenne","cases":139},{"region":"Corse","cases":69},{"region":"Franche-Comte","cases":0},{"region":"Haute-Normandie","cases":107},{"region":"Languedoc-Roussillon","cases":43},{"region":"Limousin","cases":78},{"region":"Lorraine","cases":47},{"region":"Midi-Pyrenees","cases":35},{"region":"Nord-Pas-de-Calais","cases":120},{"region":"Pays-de-la-Loire","cases":43},{"region":"Picardie","cases":0},{"region":"Poitou-Charentes","cases":129},{"region":"Provence-Alpes-Cote-dAzur","cases":23},{"region":"Ile-de-France","cases":84},{"region":"Rhone-Alpes","cases":150}]
   },
   {
+    "pos":4,
     "year":2014,
     "week":52,
     "headline": "Activité faible en augmentation",
@@ -35,6 +40,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":36},{"region":"Aquitaine","cases":170},{"region":"Auvergne","cases":85},{"region":"Basse-Normandie","cases":63},{"region":"Bourgogne","cases":58},{"region":"Bretagne","cases":72},{"region":"Centre","cases":14},{"region":"Champagne-Ardenne","cases":63},{"region":"Corse","cases":71},{"region":"Franche-Comte","cases":93},{"region":"Haute-Normandie","cases":192},{"region":"Languedoc-Roussillon","cases":49},{"region":"Limousin","cases":128},{"region":"Lorraine","cases":55},{"region":"Midi-Pyrenees","cases":36},{"region":"Nord-Pas-de-Calais","cases":145},{"region":"Pays-de-la-Loire","cases":94},{"region":"Picardie","cases":0},{"region":"Poitou-Charentes","cases":184},{"region":"Provence-Alpes-Cote-dAzur","cases":30},{"region":"Ile-de-France","cases":115},{"region":"Rhone-Alpes","cases":218}]
   },
   {
+    "pos":5,
     "year":2015,
     "week":1,
     "headline": "Activité modérée",
@@ -42,6 +48,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":84},{"region":"Aquitaine","cases":98},{"region":"Auvergne","cases":106},{"region":"Basse-Normandie","cases":107},{"region":"Bourgogne","cases":43},{"region":"Bretagne","cases":85},{"region":"Centre","cases":55},{"region":"Champagne-Ardenne","cases":174},{"region":"Corse","cases":94},{"region":"Franche-Comte","cases":68},{"region":"Haute-Normandie","cases":12},{"region":"Languedoc-Roussillon","cases":86},{"region":"Limousin","cases":182},{"region":"Lorraine","cases":122},{"region":"Midi-Pyrenees","cases":72},{"region":"Nord-Pas-de-Calais","cases":159},{"region":"Pays-de-la-Loire","cases":128},{"region":"Picardie","cases":25},{"region":"Poitou-Charentes","cases":206},{"region":"Provence-Alpes-Cote-dAzur","cases":94},{"region":"Ile-de-France","cases":110},{"region":"Rhone-Alpes","cases":166}]
   },
   {
+    "pos":6,
     "year":2015,
     "week":2,
     "headline": "Activité en nette augmentation proche du seuil épidémique",
@@ -49,6 +56,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":240},{"region":"Aquitaine","cases":229},{"region":"Auvergne","cases":202},{"region":"Basse-Normandie","cases":191},{"region":"Bourgogne","cases":119},{"region":"Bretagne","cases":127},{"region":"Centre","cases":149},{"region":"Champagne-Ardenne","cases":197},{"region":"Corse","cases":156},{"region":"Franche-Comte","cases":110},{"region":"Haute-Normandie","cases":48},{"region":"Languedoc-Roussillon","cases":108},{"region":"Limousin","cases":226},{"region":"Lorraine","cases":115},{"region":"Midi-Pyrenees","cases":93},{"region":"Nord-Pas-de-Calais","cases":178},{"region":"Pays-de-la-Loire","cases":168},{"region":"Picardie","cases":104},{"region":"Poitou-Charentes","cases":218},{"region":"Provence-Alpes-Cote-dAzur","cases":151},{"region":"Ile-de-France","cases":215},{"region":"Rhone-Alpes","cases":192}]
   },
   {
+    "pos":7,
     "year":2015,
     "week":3,
     "headline": "Activité forte",
@@ -56,6 +64,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":292},{"region":"Aquitaine","cases":165},{"region":"Auvergne","cases":314},{"region":"Basse-Normandie","cases":192},{"region":"Bourgogne","cases":96},{"region":"Bretagne","cases":137},{"region":"Centre","cases":139},{"region":"Champagne-Ardenne","cases":247},{"region":"Corse","cases":256},{"region":"Franche-Comte","cases":285},{"region":"Haute-Normandie","cases":210},{"region":"Languedoc-Roussillon","cases":176},{"region":"Limousin","cases":606},{"region":"Lorraine","cases":179},{"region":"Midi-Pyrenees","cases":197},{"region":"Nord-Pas-de-Calais","cases":164},{"region":"Pays-de-la-Loire","cases":139},{"region":"Picardie","cases":220},{"region":"Poitou-Charentes","cases":344},{"region":"Provence-Alpes-Cote-dAzur","cases":229},{"region":"Ile-de-France","cases":303},{"region":"Rhone-Alpes","cases":309}]
   },
   {
+    "pos":8,
     "year":2015,
     "week":4,
     "headline": "Activité épidémique",
@@ -63,6 +72,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":462},{"region":"Aquitaine","cases":584},{"region":"Auvergne","cases":474},{"region":"Basse-Normandie","cases":428},{"region":"Bourgogne","cases":172},{"region":"Bretagne","cases":483},{"region":"Centre","cases":308},{"region":"Champagne-Ardenne","cases":553},{"region":"Corse","cases":269},{"region":"Franche-Comte","cases":251},{"region":"Haute-Normandie","cases":293},{"region":"Languedoc-Roussillon","cases":250},{"region":"Limousin","cases":1319},{"region":"Lorraine","cases":432},{"region":"Midi-Pyrenees","cases":497},{"region":"Nord-Pas-de-Calais","cases":250},{"region":"Pays-de-la-Loire","cases":369},{"region":"Picardie","cases":768},{"region":"Poitou-Charentes","cases":437},{"region":"Provence-Alpes-Cote-dAzur","cases":292},{"region":"Ile-de-France","cases":550},{"region":"Rhone-Alpes","cases":546}]
   },
   {
+    "pos":9,
     "year":2015,
     "week":5,
     "headline": "Activité épidémique en augmentation",
@@ -70,6 +80,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":603},{"region":"Aquitaine","cases":623},{"region":"Auvergne","cases":642},{"region":"Basse-Normandie","cases":731},{"region":"Bourgogne","cases":211},{"region":"Bretagne","cases":709},{"region":"Centre","cases":730},{"region":"Champagne-Ardenne","cases":812},{"region":"Corse","cases":479},{"region":"Franche-Comte","cases":354},{"region":"Haute-Normandie","cases":673},{"region":"Languedoc-Roussillon","cases":470},{"region":"Limousin","cases":1627},{"region":"Lorraine","cases":666},{"region":"Midi-Pyrenees","cases":961},{"region":"Nord-Pas-de-Calais","cases":302},{"region":"Pays-de-la-Loire","cases":946},{"region":"Picardie","cases":705},{"region":"Poitou-Charentes","cases":727},{"region":"Provence-Alpes-Cote-dAzur","cases":675},{"region":"Ile-de-France","cases":766},{"region":"Rhone-Alpes","cases":745}]
   },
   {
+    "pos":10,
     "year":2015,
     "week":6,
     "headline": "Activité épidémique, le pic pourrait avoir été atteint",
@@ -77,6 +88,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":726},{"region":"Aquitaine","cases":782},{"region":"Auvergne","cases":825},{"region":"Basse-Normandie","cases":537},{"region":"Bourgogne","cases":321},{"region":"Bretagne","cases":709},{"region":"Centre","cases":893},{"region":"Champagne-Ardenne","cases":726},{"region":"Corse","cases":453},{"region":"Franche-Comte","cases":280},{"region":"Haute-Normandie","cases":514},{"region":"Languedoc-Roussillon","cases":723},{"region":"Limousin","cases":1848},{"region":"Lorraine","cases":833},{"region":"Midi-Pyrenees","cases":1138},{"region":"Nord-Pas-de-Calais","cases":533},{"region":"Pays-de-la-Loire","cases":944},{"region":"Picardie","cases":572},{"region":"Poitou-Charentes","cases":556},{"region":"Provence-Alpes-Cote-dAzur","cases":1075},{"region":"Ile-de-France","cases":864},{"region":"Rhone-Alpes","cases":967}]
   },
   {
+    "pos":11,
     "year":2015,
     "week":7,
     "headline": "Activité proche du pic épidémique",
@@ -84,6 +96,7 @@ var data = [
     "counts":[{"region":"Alsace","cases":677},{"region":"Aquitaine","cases":736},{"region":"Auvergne","cases":591},{"region":"Basse-Normandie","cases":426},{"region":"Bourgogne","cases":271},{"region":"Bretagne","cases":722},{"region":"Centre","cases":1063},{"region":"Champagne-Ardenne","cases":727},{"region":"Corse","cases":607},{"region":"Franche-Comte","cases":671},{"region":"Haute-Normandie","cases":586},{"region":"Languedoc-Roussillon","cases":561},{"region":"Limousin","cases":1980},{"region":"Lorraine","cases":661},{"region":"Midi-Pyrenees","cases":1013},{"region":"Nord-Pas-de-Calais","cases":448},{"region":"Pays-de-la-Loire","cases":495},{"region":"Picardie","cases":816},{"region":"Poitou-Charentes","cases":642},{"region":"Provence-Alpes-Cote-dAzur","cases":1100},{"region":"Ile-de-France","cases":824},{"region":"Rhone-Alpes","cases":962}]
   },
   {
+    "pos":12,
     "year":2015,
     "week":8,
     "headline": "Le pic épidémique a été franchi, activité en diminution",
--- js/info_narrative.js
+++ js/info_narrative.js
@@ -1,16 +1,91 @@
 
 function generateParagraphs(id,data){
-    data.forEach(function(d,i) {
-    $(id).append('<div id="para' + i + '" class="col-md-12 para">'+
-            '<span class="paraDate">' + d['year']+', semaine '+d['week']+'</span>'+
-            '<h4>' + d['headline'] + '</h4>' +
-            d['narrative'] + '</div>');
-    });
-    $(id).append('<div id="endbuffer"></div>');
+    var narrative = d3.select(".viz-content").append("div").attr("class","scrolling-center");
+    var scroll_div = d3.select(".scrolling-center")[0][0];
+    var ch_margin = {top: 10, right: 30, bottom: 20, left: 130};
+    var narrwidth = scroll_div.clientWidth-20;
+    var chartwidth = narrwidth - ch_margin.left - ch_margin.right;
+    var chartheight = data[0].counts.length*20 - ch_margin.top - ch_margin.bottom;
+    
+    var x = d3.scale.linear()
+        .domain([0,2500])
+        .range([0, chartwidth]);
+    var barHeight = 10;
+
+    var week = narrative.selectAll("div")
+        .data(data)
+        .enter()
+        .append("div")
+        .attr("class","narrative-content")
+        .attr("id", function(d,i){
+            return "narr_"+i;
+        });
+
+    var week_date = week.append("div")
+        .text(function(d){
+            return d.year+', semaine '+d.week;
+        })
+        .attr("class","narative-date");
+
+    var week_header = week.append("h3")
+        .text(function(d){
+            return d.headline;
+        });
+
+    var week_text = week.append("p")
+        .text(function(d){
+            return d.narrative;
+        });
+
+    var svg = week.append("svg")
+        .attr("width", narrwidth)
+        .attr("height", chartheight)
+        .append("g")
+        .attr("transform", "translate(" + ch_margin.left + "," + ch_margin.top + ")");
+
+    var bar = svg.selectAll("g")
+        .data(function(d){ 
+            var s = d.counts.sort(function(a,b){
+                return d3.descending(a.cases,b.cases);
+            });
+
+            var top10 = s.slice(0,10);
+            //return top10; 
+            return s;
+        })
+        .enter()
+        .append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * (barHeight+10) + ")"; });
+
+    bar.append("rect")
+        .attr("width", function(d) { ;return x(d.cases); })
+        .attr("height", barHeight)
+        .attr("class","cases")
+        .attr("fill",function(d){
+            return numtohex(d.cases);
+        });
+
+    bar.append("text")
+        .attr("x", function(d) { return x(d.cases)+3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .attr("class","barlabel")
+        .text(function(d) { return d.cases; });
+
+                
+    bar.append("text")
+    .attr("text-anchor","end")
+        .attr("x", -5)
+        .attr("y", 0)
+        .attr("dy", ".8em")
+        .attr("class","barregion")
+        .text(function(d,i){ return d.region; });  
+
 }
     
 function generateTimeline(id,data){
-    tl_margin = {top: 20, right: 10, bottom: 20, left: 10};
+
+    tl_margin = {top: 10, right: 10, bottom: 30, left: 10};
     tl_width = 50;
     tl_height = $(window).height()-tl_margin.top-tl_margin.bottom-header_height;
     var svg = d3.select("#timeline")
@@ -18,7 +93,6 @@ function generateTimeline(id,data){
             .attr("width", tl_width)
             .attr("height", tl_height+tl_margin.top+tl_margin.bottom)
             .append("g")
-            //.attr("transform", "translate(" + 10 + ",0)");
       
     var scale = d3.scale.linear()
             .range([tl_margin.top, tl_height])    
@@ -29,48 +103,44 @@ function generateTimeline(id,data){
         .enter()
         .append("text")
         .attr("y", function(d,i) {
-            return scale(i+1)+3; // scoot it over to center text
+            return scale(i+1)+3+tl_margin.top; // scoot it over to center text
         })
         .attr("x", 25)
         .attr("dx", ".35em")
         .attr("class","barlabel")
         .text(function(d,i) {
-                return d['week'] < 10 ? 's0'+d['week'] : 's'+d['week'];
+                return d['week'] < 10 ? 's0'+d.week : 's'+d.week;
         });              
 
     svg.append("line")
-        .attr("y1", tl_margin.top)
+        .attr("y1", tl_margin.top+tl_margin.top)
         .attr("x1", 20)
-        .attr("y2", tl_height)
+        .attr("y2", tl_height+tl_margin.top)
         .attr("x2", 20)
-        .attr("stroke-width", .5)
-        .attr("stroke", "black");           
+        .attr("stroke-width", .5) 
+        .attr("class","tl_line")         
     
     svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
         .attr("cy", function(d,i) {
-             return scale(i+1);
+             return scale(i+1)+tl_margin.top;
         })
         .attr("cx", function(d) {
              return 20;
         })
         .attr("r", 3.5)
         .attr("id",function(d,i){return "time_"+i;})
+        .attr("class","tl_circle")
         .attr("pos",function(d,i){return i;})
-        .attr("fill","#999999")
-        .on("click",function(){
-            if(compact){
-                showParagraph(parseInt($(this).attr('pos')),data.length);
-                updateinfographic(parseInt($(this).attr('pos')));
-            } else {
-                updateinfographic(parseInt($(this).attr('pos')));
-            }
+        .on("click",function(e){
+            //updateinfographic(e.pos);
+            updatenarrative(e.pos);
         });
         
     svg.append("circle")
-        .attr("cy", tl_margin.top)
+        .attr("cy", tl_margin.top+tl_margin.top)
         .attr("cx", function(d) {
              return 20;
         })
@@ -80,69 +150,6 @@ function generateTimeline(id,data){
         .attr("stroke", "rgb(50,160,160)") 
         .attr("fill-opacity","0.3")
         .attr("fill","rgb(50,160,160)");         
-}
-
-function generateBarChart(id,datain){
-    
-    var data;
-    var s = datain['counts'].sort(function(a,b){
-        return d3.descending(a.cases,b.cases);
-    });
-
-    data = s.slice(0,10);
-
-    var margin = {top: 10, right: 30, bottom: 20, left: 130},
-    width = $(id).width() - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;    
-    var barHeight = (height)/data.length-10;   
-
-    var x = d3.scale.linear()
-        .domain([0,2500])
-        .range([0, width]);
-
-    var svg  = d3.select(id)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var bar = svg .selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * (barHeight+10) + ")"; });
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    bar.append("rect")
-        .attr("width", function(d) { return x(d['cases']); })
-        .attr("height", barHeight)
-        .attr("class","cases");
-
-    bar.append("text")
-        .attr("x", function(d) { return x(d['cases'])+3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .attr("class","barlabel")
-        .text(function(d) { return d['cases']; });
-
-                
-    bar.append("text")
-        .attr("x", -130)
-        .attr("y", 0)
-        .attr("dy", ".58em")
-        .attr("class","barregion")
-        .text(function(d,i){ return d['region']; });                
-
-    if (!compact){
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-    }
-    
 }
 
 function generateMap(id){
@@ -164,13 +171,13 @@ function generateMap(id){
     var g = svg.append("g");
     
     g.selectAll("path")
-      .data(fr_regions.features)
-          .enter()
-          .append("path")
-          .attr("d", path)
-          .attr("id",function(d,i){return d.properties.ADM1_NAME;})
-          .attr("class","region")
-          .attr("fill","transparent");        
+        .data(fr_regions.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("id",function(d,i){return d.properties.ADM1_NAME;})
+        .attr("class","region")
+        .attr("fill","transparent");        
 }
 
 function highlighttimeline(id,num){ 
@@ -182,7 +189,7 @@ function highlighttimeline(id,num){
     d3.select('#selectedcircle')
         .transition()
         .attr("cy", function(d,i) {
-             return scale(num+1);
+             return scale(num+1)+tl_margin.top;
         })
         
 }
@@ -194,7 +201,7 @@ function highlightmap(num){
     });
 }
 
-function numtohex(num,limit){
+function numtohex(num){
     var color;
 
     if(num<25){
@@ -227,189 +234,72 @@ function numtohex(num,limit){
     return color;
 
 }
-            
-/////////////////////////////////////
-//functions for expanded view only
-////////////////////////////////////            
+
+// Tween stuff for scroll to funcitons
+
+function doScroll(wkNum){
+    var elScroll = d3.select("#narr_"+wkNum)[0][0].offsetTop;
+    d3.transition()
+        .delay(200)
+        .duration(1500)
+        .tween("scroll", scrollTween(elScroll));
+};
+
+function scrollTween(scrollTop) {
+  return function() {
+    var i = d3.interpolateNumber(window.pageYOffset || document.documentElement.scrollTop, scrollTop);
+    return function(t) { scrollTo(0, i(t)) };
+ };
+}
+   
+
+// functions for paragraph scroll
+$(window).scroll(function(){
+    updateinfographic(getParagraphInView(data.length,125));
+});    
             
 function getParagraphInView(numparas,mar){
     var parainview=0;
+
     for(i=0;i<numparas;i++){
-        if($('#para'+i).offset().top-mar<=$(window).scrollTop()){
+        var ot = d3.select('#narr_'+i)[0][0].offsetTop;
+        var st = $(window).scrollTop();
+        if(ot-mar<=st && Math.abs(st-mar)>200){
             parainview=i;
         }
     }
     return parainview;
 }
-            
-function stickydiv(){
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top){
-        $('#info-graphic').addClass('sticky');
-    }
-    else{
-        $('#info-graphic').removeClass('sticky');
-    }
-};
 
-///////////////////////////////////////
-////// Functions for compact view
-///////////////////////////////////////
 
-function showParagraph(id,numparas){
-    for(i=0;i<numparas;i++){
-        $('#para'+i).hide();
-    }
-    $('#endbuffer').hide();
-    $('#para'+id).show();
-}
-
-///////////////////////////////////////
-////// Transition infogrpahic functions
-///////////////////////////////////////
+// Transition infogrpahic functions
 
 function updateinfographic(temppara){
-    var match = data.filter(function(d,i) { return i == temppara; })[0];
-
     if(currentpara!==temppara){
         highlighttimeline('#timeline',temppara);
-        d3.select('#timelinedate'+currentpara).classed('hidden', true);
-        d3.select('#timelinedate'+temppara).classed('hidden', false);
-        $('#timelinedate'+temppara).removeClass('hidden');
-        $('#barcharttitle').html('Cas confirmés par 100 000 personnes (' + match.year+', semaine '+match.week+')');
-        if(!compact){
-            $('#para'+currentpara).removeClass('highlightedpara');
-            $('#para'+temppara).addClass('highlightedpara');
-        }
-        transitionBarChart('#barchart',data[temppara]);
         highlightmap(temppara);
         currentpara=temppara;
-        
+    }
+}
+function updatenarrative(temppara){
+    if(currentpara!==temppara){
+        doScroll(temppara);
+        //currentpara=temppara;
     }
 }
 
-function transitionBarChart(id,datain){
-    var data;
-    var s = datain['counts'].sort(function(a,b){
-        return d3.descending(a.cases,b.cases);
-    })
-    data = s.slice(0,10);
 
-    var margin = {top: 10, right: 30, bottom: 20, left: 135},
-    width = $(id).width() - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;    
-    var barHeight = (height)/data.length-10;   
 
-    var x = d3.scale.linear()
-        .domain([0,2500])
-        .range([0, width]);
+// initialization
 
-    d3.select(id).selectAll("rect")
-        .data(data).transition()
-        .duration(1000)
-        .attr("width", function(d) { return x(d['cases']); })
-        .attr("height", barHeight);
-
-    d3.select(id).selectAll(".barlabel")
-        .data(data).transition()
-        .duration(1000)
-        .attr("x", function(d) { return x(d['cases'])+5; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d['cases']; });
-
-    d3.select(id).selectAll(".barregion")
-        .data(data).transition()
-        .duration(1000)
-        .text(function(d,i){ return d['region']; }); 
-}
-
-/////////////////////////////////////////
-////// Window resize events
-////////////////////////////////////////
-
-function resizedw(){
-    if($(window).width()<768){compact = true;} else {compact = false;}
-    if($(window).width()<currentwidth-20 || $(window).width()>currentwidth+20){
-        currentwidth = $(window).width();
-        $('#text').html('');
-        $('#timeline').html('');
-        $('#barchart').html('');
-        $('#map').html('');
-        $('#info-graphic').removeClass('sticky');
-        generateParagraphs('#text',data);
-        generateTimeline('#timeline',data);
-        generateBarChart('#barchart',data[0]);
-        generateMap('#map');
-        var temppara=currentpara;
-        currentpara=-1;
-        updateinfographic(temppara);
-        if(compact){
-            showParagraph(temppara,42);
-            $('html, body').animate({
-                scrollTop: 0
-            }, 500);
-            $('#browse').show();
-        } else {
-            $('html, body').animate({
-                scrollTop: $('#para'+temppara).offset().top
-            }, 500);
-            $('#browse').hide();
-        }
-        highlighttimeline('#timeline',temppara);
-    }
-}
-
-//////////////////////////////
-///// initialisation
-/////////////////////////////
-
-var compact = false;
-var currentwidth=$(window).width();
 var currentpara = -1;
 var tl_width, tl_height,tl_margin;
 var header_height = 75;
-
-if($(window).width()<768){compact = true;}            
+          
 generateParagraphs('#text',data);
 generateTimeline('#timeline',data);
-generateBarChart('#barchart',data[0]);
 generateMap('#map');
 updateinfographic(0);
 
 
 
-$(window).scroll(function(){
-    if(!compact){
-        stickydiv();
-        updateinfographic(getParagraphInView(data.length,150));
-    }
-});
-
-if(compact){    
-    showParagraph(0,data.length);
-} else {
-    $('#browse').hide();
-};
-
-var doit;
-
-window.onresize = function(){
-  clearTimeout(doit);
-  doit = setTimeout(resizedw, 100);
-};
-
-$('#Next').on("click",function(){
-    if(currentpara<data.length){
-        showParagraph(currentpara+1,data.length);
-        updateinfographic(currentpara+1);
-    }
-});
-
-$('#Previous').on("click",function(){
-    if(currentpara>0){
-        showParagraph(currentpara-1,data.length);
-        updateinfographic(currentpara-1);
-    }
-});
